f66a951a2229eefedb58bcfef0593b81f927302c
Jia Rong <jr.loves.maths@gmail.com>
2016-10-10 10:50:32 +0800

    Make redux persist work with immutable js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import configureStore from './store/configureStore';
+import immutableTransform from 'redux-persist-transform-immutable'
 import { persistStore, autoRehydrate } from 'redux-persist';
 import './index.css';
 
 const store = configureStore();
 
-// persistStore(store);
+persistStore(store, { transforms: [immutableTransform()] });
 
 const Wrapper = () => {
   return (
